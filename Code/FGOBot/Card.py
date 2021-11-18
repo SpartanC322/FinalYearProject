3aa7d79d7159655948b31d4e2edcfc3c00459dda
Ben Lenihan <benlenihan99@gmail.com>
2021-11-18 22:44:50 +0000

    Additions to code

---
+++ Code/FGOBot/Card.py
@@ -0,0 +1,18 @@
+class Card:
+    card = "Empty Card"
+
+
+class Buster(Card):
+    card = "Buster Card"
+
+
+class Arts(Card):
+    card = "Arts Card"
+
+
+class Quick(Card):
+    card = "Quick Card"
+
+
+class Extra(Card):
+    card = "Extra Card"
